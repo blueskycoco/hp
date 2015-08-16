afd9df4973c7b8869cfb846c82eaf8114865f234
U-XBL9VNFWMJBYVNH\Administrator <Administrator@XBL9VNFWMJBYVNH.(none)>
2015-08-17 06:39:33 +0800

    update web process

--- web/web_interface.c
+++ web/web_interface.c
@@ -1,5 +1,7 @@
 #include "web_interface.h"
+#include "weblib.h"
 
+char g_url[10][256]={0};
 int send_msg(int msgid,unsigned char msg_type,unsigned char id,unsigned char *text)
 {
 	struct msg_st data;
@@ -21,20 +23,94 @@ int send_msg(int msgid,unsigned char msg_type,unsigned char id,unsigned char *te
 	}
 	printf(LOG_PREFX"send msg done\n");
 }
-int send_web(char *url,char *commandid,char *lampcode,int timeout)
+int send_web(char *url,char *commandid,char *message,int timeout)
 {
-	char *rcv=http_get(argv[2]/*"http://101.200.236.69:8080/lamp/lamp/commond/wait?lampCode=aaaa"*/,atoi(argv[1]));
+	char *request[1024]={0};
+	int result=0;
+	sprintf(request,"%s?%s&%s",url,commandid,message);
+	char *rcv=http_get(request,timeout);
 	if(rcv!=NULL)
 	{
 		printf("%s\n",rcv);
-		res=doit(rcv,"code");
+		char *res=doit(rcv,"code");
 		if(res)
 		{
 			printf(LOG_PREFX"code is %s\n",res);
+			if(atoi(res)==0)
+				result=1;
 			free(res);
 		}
 		free(rcv);
 	}
+	return result;
+}
+void get_param(char *input,char *message,char *commandid,int *websiteid,int *timeout)
+{
+	int j=0;
+	int i=strlen(input)-1;
+	memset(message,'\0',sizeof(message));
+	memset(commandid,'\0',sizeof(commandid)); 				
+	strcpy(message,"message=");
+	strcpy(commandid,"commandID=");
+	*timeout=-1;
+	*websiteid=-1;
+	*timeout=atoi(strrchr(input,';')+1);
+	 while(input[i]!=';' && i=0)
+	 	i--;
+	 i--;
+	 *websiteid=input[i]-48;
+	 i=i-2;
+	 j=i;
+	 while(input[i]!=';' && i=0)
+	 	i--;
+	 i--;
+	 memcpy(commandid+strlen("commandID="),input+i,j-i+1);
+	 memcpy(message+strlen("message="),input,i-1);
+}
+int get_server_cmd(char *url)
+{
+	int result=0;
+	char *text_out[512]={0};
+	char *rcv=http_get(url,3);
+	if(rcv!=NULL)
+	{
+		printf("%s\n",rcv);
+		char *res=doit(rcv,"code");
+		if(res)
+		{
+			printf(LOG_PREFX"code is %s\n",res);
+			if(atoi(res)==0)
+				result=1;
+			free(res);
+		}
+		if(result)
+		{
+			char *lampCode=doit(rcv,"commandid");
+			char *commandid=doit(rcv,"message");
+			char *commandCode=doit(rcv,"commandCode");
+			memset(text_out,'\0',sizeof(text_out));
+			if(commandCode)
+			{
+				memcpy(text_out,commandCode,3);
+				strcat(text_out,"w;");			
+				strcat(text_out,commandCode+3);
+			}
+			if(lampCode)
+			{
+				strcat(text_out,";");
+				strcat(text_out,lampCode);
+			}
+			if(commandid)
+			{
+				strcat(text_out,";");
+				strcat(text_out,commandid);
+			}
+			if(strlen(text_out)!=0)
+				send_msg(msgid,TYPE_WEB_TO_MAIN,WEB_TO_MAIN,text_out);
+		}
+		free(rcv);
+	}
+	return result;
 }
 int main(int argc, char *argv[])
 {
@@ -66,30 +142,63 @@ int main(int argc, char *argv[])
 	fpid=fork();
 	if(fpid==0)
 	{
-
+		get_server_cmd(g_url[0]);
 	}
 	else if(fpid>0)
 	{
 		char lampcode[256]={0};
 		char commandid[256]={0};
+		char errorMsg[256]={0};
 		int websiteid=0;
 		int timeout=0;
-		char key[256]={0};
-		char value[256]={0};
+		int i=0,j=0;
+		char message[256]={0};
 		while(1)
 		{
 			printf(LOG_PREFX"waiting MainCtlSystem cmd...\n");
-			if(msgrcv(msgid, (void*)&data, sizeof(struct msg_st)-sizeof(long int), TYPE_MAIN_TO_FILE , 0)>=0)
-			{
+			if(msgrcv(msgid, (void*)&data, sizeof(struct msg_st)-sizeof(long int), TYPE_MAIN_TO_WEB , 0)>=0)
+			{			
+				memset(errorMsg,'\0',256);
+				strcpy(errorMsg,data.text);
 				if(data.id==MAIN_TO_WEB)
 				{
-					if(data.text[0]=='0' && data.text[1]=='1')
+					if(data.text[0]=='f' && data.text[1]=='f')
 					{
-						
+						int m=3;
+						for(i=0;i<10;i++)
+						{
+							j=m;
+							while(data.text[m]!=';' && data.text[m]!='\0')
+								m++;
+							if(data.text[m]=='\0')
+								break;
+							else
+							{								
+								memset(g_url[i],'\0',256);
+								memcpy(g_url[i],data.text+j,m-j);
+								m++;
+							}
+						}
+						strcat(errorMsg,";done");
+						send_msg(msgid,TYPE_WEB_TO_MAIN,WEB_TO_MAIN,errorMsg);
 					}
-					else if(data.text[0]=='0' && data.text[1]=='2')
+					else
+					{
+						get_param(data.text,message,commandid,&websiteid,&timeout);
+						printf(LOG_PREFX"input %s\nmessage %s\ncommandid %s\nwebsiteid %d\ntimeout %d\n",
+							data.text,message,commandid,websiteid,timeout);
+						if(send_web(g_url[websiteid],commandid,message,timeout))
+							strcat(errorMsg,";done");
+						else
+							strcat(errorMsg,";failed");
+							send_msg(msgid,TYPE_WEB_TO_MAIN,WEB_TO_MAIN,errorMsg);
+					}
 				}
-				
+				else
+				{
+					strcat(errorMsg,";errorID");
+					send_msg(msgid,TYPE_WEB_TO_MAIN,WEB_TO_MAIN,errorMsg);
+				}
 			}
 			else
 			{
--- web/web_interface.h
+++ web/web_interface.h
@@ -12,47 +12,6 @@
 #define MAIN_TO_WEB 					0x03
 #define WEB_TO_MAIN 					0x01
 #define LOG_PREFX 						"[WebSubSystem]:"
-#define CMD_00_MUSIC_FIND				"r;00;b;??"
-#define CMD_01_MUSIC_PLAY				"r;01;"
-#define CMD_01_MUSIC_PLAY_WEB			"r;01;w;??;"
-#define CMD_03_MUSIC_CHOOSE				"w;03;?;??;"
-#define CMD_04_MUSIC_LIKE				"w;04;?;02;"
-#define CMD_05_MUSIC_UNLIKE				"w;05;?;02;"
-#define CMD_07_LIGHT_MANUAL				"w;07;?;00;"
-#define CMD_11_LIGHT_MODE_FIND			"r;11;?;00"
-#define CMD_12_LIGHT_MODE_CHECKs		"r;12;?;00;"
-#define CMD_13_LIGHT_MODE_CHOOSE		"r;13;?;00;"
-#define CMD_14_LIGHT_MODE				"r;14;?;00;"
-#define CMD_15_SAVE_MODE_ON_WEB			"w;15;?;00;040;"
-#define CMD_15_SAVE_MODE_ON				"w;15;?;00;040"
-#define CMD_16_SAVE_MODE_OFF_WEB		"w;16;?;00;040;"
-#define CMD_16_SAVE_MODE_OFF			"w;16;?;00;040"
-#define CMD_17_RING_SET					"w;17;?;04;?"//?
-#define CMD_17_RING_SET_2				"w;17;?;"//?
-#define CMD_18_RING_ON					"w;18;?;04;"
-#define CMD_19_RING_OFF					"w;19;?;04;"
-#define CMD_20_RING_DELAY				"r;20;04;"
-#define CMD_21_RING_NOW_ARM				"r;21;00;"
-#define CMD_21_RING_NOW_ARM_2			"r;21;04;"
-#define CMD_23_TIME_UPDATE_ARM			"a;23;05;"
-#define CMD_24_ADD_REMINDER				"w;24;?;03;?"//?
-#define CMD_24_ADD_REMINDER_2			"w;24;?;03;"//?
-#define CMD_25_REMINDER_ON_ARM			"r;25;03;"
-#define CMD_28_REMINDER_ON_ARM			"r;28;b;04"
-
-#define FILE_PATH_NAME "/home/file/"
-
-#define FILE_MUSIC_NAME_LINE	0
-#define FILE_MUSIC_SINGER_LINE	1
-#define FILE_MUSIC_PATH_LINE	2
-#define FILE_MUSIC_LIKE_LINE	3
-#define FILE_MUSIC_LINES_RECORD	4
-#define FILE_LIGHT_LINES_RECORD	7
-#define FILE_LIGHT_NAME_LINE	0
-#define FILE_LIGHT_MODE_USE		6
-#define FILE_ALARM_LINES_RECORD	9
-#define FILE_ALERT_LINES_RECORD	5
-
 struct msg_st
 {  
 	long int msg_type; 
